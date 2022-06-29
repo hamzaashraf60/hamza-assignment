45edbb247763873b5aaed71490e17c170a57f32a
Ali Hassan <Ali Hassan@DESKTOP-K9MLS2L>
2022-06-29 11:02:51 +0500

    Add project files.

---
+++ Hamza assignment/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hamza_assignment.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult contact()
+        {
+            return View();
+        }
+    }
+}
